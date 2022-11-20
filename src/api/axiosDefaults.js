d86057173a0bcaa8cdd2cf5559dc69687b41f30f
Jordan <jrdnbrkfld@icloud.com>
2022-11-20 14:21:27 +0000

    corrected baseURL, had the incorrect URL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://moments-project-1.herokuapp.com/";
+axios.defaults.baseURL = "https://jordan-drf-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
